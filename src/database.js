1c263e88a47a1d3fac96051fdf4b0f3290433320
Mario <elasgard@hotmail.es>
2019-05-19 23:29:42 +0200

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 
-// Database Connection for Production
-
+// database connection for deploying in Cloud Shell
 let config = {
     //host: "104.197.131.49",
     //host: "35.241.174.226",
@@ -10,17 +9,10 @@ let config = {
     password: "suilluss",
 }
 
-// let config = {
-//     user: process.env.SQL_USER,
-//     database: process.env.SQL_DATABASE,
-//     password: process.env.SQL_PASSWORD,
-// }
-
-if ("innovati-2:europe-west1:mydb" && process.env.NODE_ENV === 'production') {
+if ("innovati-2:europe-west1:mydb" && process.env.NODE_ENV === "production") {
     config.socketPath = `/cloudsql/innovati-2:europe-west1:mydb`;
 }
 
-// let connection = mysql.createConnection(config);
-
 let connection = mysql.createConnection(config);
+
 module.exports = connection;
